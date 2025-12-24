de6ed89feb25dbedbb49a8c600f0e2ba800528cc
Trackrabbit <ajamespage@gmail.com>
2025-12-24 10:47:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
   <link
     rel="stylesheet"
     href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-    integrity="sha256-p4NxAoJBhIIN+hmNHrEHI6lHDBcdoU2S2mGm6H3/tE8="
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
     crossorigin=""
   />
 
@@ -92,11 +92,12 @@
 
   <!-- Leaflet JS -->
   <script
-    src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-    crossorigin=""
+  src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+  integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+  crossorigin=""
   ></script>
 
   <script src="app.js"></script>
 </body>
 </html>
+
